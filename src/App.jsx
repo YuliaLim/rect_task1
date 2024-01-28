5396312e74b2a14b7b91e915c196d09024ff99c3
YuliaLim <uli4ka.lmarenko@gmail.com>
2024-01-28 19:22:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Photo from './components/photo'
+import Name from './components/name'
+
+function App() {
+
+  return (
+    <div>
+      <Photo />
+      <Name />
+    </div>
+  )
+
+}
+
+export default App
